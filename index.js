57ea5b0886c778a9d3b07c8de9f7afe93c8c1483
Oscar <oscarkimani86@icloud.com>
2024-04-16 07:45:00 +0300

    array find method

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+const record = [
+    { year: 2016, result: 'L' },
+    { year: 2015, result: 'W' },
+    { year: 2014, result: 'L' },
+  ];
+  
+  console.log(superbowlWin(record)); 
+
+  function superbowlWin(record) {
+    
+    const win = record.find(obj => obj.result === 'W');
+  
+    
+    if (win) {
+      return win.year;
+    }
+  
+    
+    return undefined;
+  }
